db4aa974b182f980dc9d94645243e111bc599d94
João Rafael Fagundes Menezes <joaorafael652@outlook.com>
2019-08-12 17:03:20 -0300

    Criação de AutoMapper, Services e ViewModel.
    Produto e Categoria.

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.dimensoes.largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.dimensoes.altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.dimensoes.profundidade));
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Application/Services/ProdutoAppService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.DomainObject;
+
+namespace NerdStore.Catalogo.Application.Services
+{
+    public class ProdutoAppService : IProdutoAppService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IMapper _mapper;
+        private readonly IEstoqueService _estoqueService;
+
+        public ProdutoAppService(IProdutoRepository produtoRepository, 
+                                IMapper mapper, 
+                                IEstoqueService estoqueService)
+        {
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
+            _estoqueService = estoqueService;
+        }
+
+        public async Task AdicionarProduto(ProdutoViewModel produtoViewModel)
+        {
+            var produto = _mapper.Map<Produto>(produtoViewModel);
+            _produtoRepository.Adicionar(produto);
+
+            await _produtoRepository.unitOfWork.Commit();
+        }
+
+        public async Task AtualizarProduto(ProdutoViewModel produtoViewModel)
+        {
+            var produto = _mapper.Map<Produto>(produtoViewModel);
+            _produtoRepository.Atualizar(produto);
+
+            await _produtoRepository.unitOfWork.Commit();
+        }
+
+        public async Task<ProdutoViewModel> DebitarEstoque(Guid id, int quantidade)
+        {
+            if (!_estoqueService.DebitarEstoque(id, quantidade).Result)
+                throw new DomainException("Falha ao debitar estoque de produto.");
+
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterPorID(id));
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository?.Dispose();
+            _estoqueService?.Dispose();
+        }
+
+        public async Task<IEnumerable<CategoriaViewModel>> ObterCategorias()
+        {
+            return _mapper.Map<IEnumerable<CategoriaViewModel>>(await _produtoRepository.ObterCategorias());
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo)
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterPorCategoria(codigo));
+        }
+
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterPorID(id));
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos());
+        }
+
+        public async Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade)
+        {
+            if (!_estoqueService.ReporEstoque(id, quantidade).Result)
+                throw new DomainException("Falha ao repor estoque do produto.");
+
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterPorID(id));
+        }
+    }
+}
